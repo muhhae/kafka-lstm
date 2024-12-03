7dfbe60948424a5cc4742318971aff2472e6a2dd
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2024-12-03 18:20:31 +0700

    init

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Data struct {
+	Value     float32   `json:"value"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func main() {
+	writers := []*kafka.Writer{
+		{
+			Addr:                   kafka.TCP("localhost:9092"),
+			Topic:                  "temp_1",
+			AllowAutoTopicCreation: true,
+		}, {
+			Addr:                   kafka.TCP("localhost:9092"),
+			Topic:                  "temp_2",
+			AllowAutoTopicCreation: true,
+		}, {
+			Addr:                   kafka.TCP("localhost:9092"),
+			Topic:                  "humid_1",
+			AllowAutoTopicCreation: true,
+		}, {
+			Addr:                   kafka.TCP("localhost:9092"),
+			Topic:                  "humid_2",
+			AllowAutoTopicCreation: true,
+		}, {
+			Addr:                   kafka.TCP("localhost:9092"),
+			Topic:                  "light_intensity",
+			AllowAutoTopicCreation: true,
+		},
+	}
+
+	url := "https://ssiot.jlbsd.my.id/api/all"
+	timeLayout := "2006-01-02 15:04:05"
+
+	now := time.Now().UnixMilli()
+	var prev int64 = 0
+
+	for {
+		if now = time.Now().UnixMilli(); now-prev < 1000 {
+			continue
+		}
+		prev = now
+		response, err := http.Get(url)
+
+		if response.StatusCode != http.StatusOK {
+			log.Printf("Failed To Fetch Error: ", response.StatusCode)
+			continue
+		}
+
+		defer response.Body.Close()
+
+		var result []Data
+		err = json.NewDecoder(response.Body).Decode(&result)
+		if err != nil {
+			log.Printf("Failed to Decode Error: ", err.Error())
+			continue
+		}
+		fmt.Println(result)
+
+		for i, w := range writers {
+			err = w.WriteMessages(context.Background(),
+				kafka.Message{
+					Key:   []byte("value"),
+					Value: []byte(fmt.Sprintf("%f", result[i].Value)),
+				},
+				kafka.Message{
+					Key:   []byte("timestamp"),
+					Value: []byte(result[i].Timestamp.Format(timeLayout)),
+				},
+			)
+			if err != nil {
+				log.Printf("Failed to Publish: ", err.Error())
+				break
+			}
+		}
+	}
+}
